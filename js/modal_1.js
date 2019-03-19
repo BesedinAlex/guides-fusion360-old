3a7ecc7dae90644b5475a95e478cbcb50f4a5db6
PenguinAlex <besedinalexey@gmail.com>
2019-03-19 20:31:03 +0300

    00001

--- js/modal_1.js
+++
@@ -1,7 +0,0 @@
-$('.btnNext').click(function() {
-$('.nav-tabs .active').parent().next('li').find('a').trigger('click');
-});
-
-$('.btnPrevious').click(function() {
-$('.nav-tabs .active').parent().prev('li').find('a').trigger('click');
-});
