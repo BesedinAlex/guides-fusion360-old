7d703141bd6bfa7dc0e76adbe4313d84ac5af8a6
Alexey <besedinalexey@gmail.com>
2019-05-16 14:16:04 +0300

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {GuidesComponent} from './guides/guides.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: GuidesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
